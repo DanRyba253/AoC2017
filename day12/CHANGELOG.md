fb2ace80a1003bcda47623c3335ffcfbdc941f87
Dani Rybe <danilrybakov249@gmail.com>
2024-07-03 22:34:21 +0700

    day 12

---
+++ day12/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for AoC2017Day12
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ day12/app/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import           Data.List   (find)
+import           Data.Set    (Set)
+import qualified Data.Set    as S
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+
+connected :: Vector [Int] -> Set Int -> Int -> Set Int
+connected graph visited current
+    | S.member current visited = visited
+    | otherwise = foldl (connected graph) (S.insert current visited) (graph V.! current)
+
+groupCount :: Vector [Int] -> Set Int -> Int -> Int
+groupCount graph visited current = 1 + maybe 0
+    (groupCount graph visited')
+    (find (`S.notMember` visited') allNodes)
+  where
+    visited' = connected graph visited current
+    allNodes = [0 .. V.length graph - 1]
+
+main :: IO ()
+main = do
+    graph <- V.fromList . read <$> readFile "input.txt"
+    putStrLn $ "Part 1: " ++ show (S.size $ connected graph S.empty 0)
+    putStrLn $ "Part 2: " ++ show (groupCount graph S.empty 0)
